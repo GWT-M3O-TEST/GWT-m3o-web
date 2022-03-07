ee7bafb564bf871d7cc7a94ab7ef1ce0c0720c8b
Asim Aslam <asim@aslam.me>
2022-03-07 09:24:11 +0000

    .

---
+++ client/index.js
@@ -0,0 +1,20 @@
+class Client {
+  constructor(token) {
+    this.token = token;
+  }
+
+  call(service, endpoint, request, callback) {
+    // e.g /v1/helloworld/Call
+    var path = "/v1/" + service + "/" + endpoint
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4);
+      callback(JSON.parse(xmlHttp.responseText), xmlHttp.status);
+    };
+    xmlHttp.open("POST", "https://api.m3o.com" + path, true); // true for asynchronous
+    xmlHttp.setRequestHeader("Authorization", "Bearer " + this.token);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(request);
+  }
+}
